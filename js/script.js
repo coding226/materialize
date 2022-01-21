b462546c7c4afd6675a5438b3c490812c6854fdf
codinglovely226 <codinglovely226@gmail.com>
2022-01-21 21:48:41 +0800

    third commit

--- js/script.js
+++ js/script.js
@@ -12,12 +12,12 @@ $(document).ready(function(){
         var tr;
         $('.year-filter').html('');
         tr = $('<ul class="pagination" />');
-         tr.append("<td class=\"waves-effect\"><a href=\"#!\" class='btn waves-effect waves-light'>" + (this_year + 1) + "</a></td>");
-         tr.append("<td class=\"waves-effect\"><a href=\"#!\" class='btn waves-effect waves-light'>" + this_year + "</a></td>");
-         tr.append("<td class=\"waves-effect\"><a href=\"#!\" class='btn waves-effect waves-light'>" + (this_year - 1) + "</a></td>");
-         tr.append("<td class=\"waves-effect\"><a href=\"#!\" class='btn waves-effect waves-light'>" + (this_year - 2) + "</a></td>");
-         tr.append("<td class=\"waves-effect\"><a href=\"#!\" class='btn waves-effect waves-light'>" + (this_year - 3) + "</a></td>");
-         tr.append("<td class=\"waves-effect\"><select class='pp'></select></td>");
+         tr.append("<td class=\"\"><a href=\"#!\" class='btn'>" + (this_year + 1) + "</a></td>");
+         tr.append("<td class=\"\"><a href=\"#!\" class='btn'>" + this_year + "</a></td>");
+         tr.append("<td class=\"\"><a href=\"#!\" class='btn'>" + (this_year - 1) + "</a></td>");
+         tr.append("<td class=\"\"><a href=\"#!\" class='btn'>" + (this_year - 2) + "</a></td>");
+         tr.append("<td class=\"\"><a href=\"#!\" class='btn'>" + (this_year - 3) + "</a></td>");
+         tr.append("<td class=\"\"><select class='pp'></select></td>");
         $('.year-filter').append(tr);
          var filter_year = (this_year - 4)
         var se;
